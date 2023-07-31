ea6153df74962c0e16c984c16ccbf78bf8bb5399
easpace <140628464+easpace@users.noreply.github.com>
2023-07-31 17:49:05 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,6 +33,6 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
 #Allow the end user to add a fruit to the list
-add_my_fruit = streamlit.write('What fruit would you like to add? ', fruit_choice)
+add_my_fruit = streamlit.text_input('What fruit would you like to add? ', fruit_choice)
 streamlit.write('Thanks for adding ', add_my_fruit)
 
