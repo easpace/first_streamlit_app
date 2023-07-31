8b3da9953f10a465e699f9c2b7a1782981516015
easpace <140628464+easpace@users.noreply.github.com>
2023-07-31 18:19:02 +0800

    Update streamlit_app.py
    my_cur.execute

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,3 +36,5 @@ streamlit.dataframe(my_data_rows)
 add_my_fruit = streamlit.text_input('What fruit would you like to add? ', fruit_choice)
 streamlit.write('Thanks for adding ', add_my_fruit)
 
+my_cur.execute("insert into fruit_load_list values ('from steramlit')")
+
